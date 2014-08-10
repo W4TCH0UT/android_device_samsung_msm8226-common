7f3665c4c056a585da197842d44fe158e454fd0d
Mukul Soni <mukulsoni2@gmail.com>
2014-08-11 02:24:03 +0700

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,8 @@ WLAN_MODULES:
 	mkdir -p $(KERNEL_MODULES_OUT)/pronto
 	mv $(KERNEL_MODULES_OUT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
 	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/wlan.ko
-	TARGET_KERNEL_MODULES += WLAN_MODULES
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
 
 # QCOM BSP
 TARGET_USES_QCOM_BSP := true
